6275d59ec247123a652f098a480bff24c13c08fd
shamikae <e.shamika@yahoo.com>
2021-10-08 01:14:06 -0400

    pass by reference

--- Memory/reference.cpp
+++ Memory/reference.cpp
@@ -11,7 +11,16 @@ Memory Address
 which is the location of where the variable is stored on the computer.
 
 *give you the ability to manipulate the data in the computer's memory - which can reduce the code and improve the performance.
+
+Pass By Reference
+You can also pass a reference to the function. This can be useful when you need to change the value of the arguments:
 */
+
+void swapNums(int &x, int &y) {
+  int z = x; //holds the value of x(10)
+  x = y; // x holds the value of y(20)
+  y = z; //y becomes the value of z(10)
+}
 
 int main(){
     string food = "Pizza";// food variable
@@ -20,5 +29,18 @@ int main(){
     cout << food << "\n"; // Outputs Pizza
     cout << meal << "\n"; // Outputs Pizza
     cout << &food << "\n";// Outputs 0x7ffeec37 as memory address
+
+    int firstNum = 10;
+    int secondNum = 20;
+
+  cout << "Before swap: " << "\n";
+  cout << firstNum << secondNum << "\n";
+
+  // Call the function, which will change the values of firstNum and secondNum
+  swapNums(firstNum, secondNum);
+
+  cout << "After swap: " << "\n";
+  cout << firstNum << secondNum << "\n";
+
     return 0;
 }
