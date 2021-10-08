6dc3125b8b398f2353946909f71bb1cebd990786
shamikae <e.shamika@yahoo.com>
2021-10-08 02:14:15 -0400

    Classes

--- OOP/classes.cpp
+++ OOP/classes.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
 using std::string;
+using std::cout;
+/*
+A class is a user-defined data type that we can use in our program, and it works as an object constructor, or a "blueprint" for creating objects.
+Class is a template for objects , and an object is an instance of a class.
+When the individual object is created they inherit all the variables and functions from the class.
 
-class Employee { //private by default
+example: in real life, a car is an object. The car has attributes, such as weight and color, and methods, such as drive and brake.
+Attributes and methods are basically variables and functions that belongs to the class. These are often referred to as "class members".
+*/
+
+class MyClass {
+    public: 
+        int myNum;
+        string myString;
+        string paramMethod(string param);
+};
+
+string MyClass::paramMethod(string param) {
+    return param;
+}
+
+class Employee { //Access specifier private by default
 public:
-    string Name;
-    string Company;
-    int Age;
+    string Name; //attribute (string variable)
+    string Company; 
+    int Age; //attribute (int variable)
 
-    void IntroduceYourself(){
+    void IntroduceYourself(){  //Method(function) defined inside the class
         std::cout << "Name - " << Name << std::endl;
         std::cout << "Company - " << Company << std::endl;
         std::cout << "Age - " << Age << std::endl;
 
     }
+
+    void method(); //method(function) declaration
+
     /* When creating custom constructor, the default constructor no longer works*/
     Employee(string name, string company, int age) {
         Name = name;
@@ -22,9 +45,27 @@ public:
     }
 };
 
+void Employee::method() {
+    cout << "Hello, Welcome!" <<"\n"; //Method(function) definition outside the class
+}
+
 int main() {
     Employee employee1 = Employee("Sam", "Self", 31);
-    employee1.IntroduceYourself();
+    employee1.IntroduceYourself(); //Call method
+    employee1.method();
+
+    MyClass myObj;
+    myObj.myNum = 23;
+    myObj.myString = "String";
+    cout << myObj.myNum << "\n";
+    cout << myObj.myString << "\n";
+
+    MyClass myObj1;
+    myObj1.myNum = 100;
+    myObj1.myString = "Second String";
+    cout << myObj1.myNum << "\n";
+    cout << myObj1.myString << "\n";
+    cout << myObj1.paramMethod("This is a parameter added to a method") << "\n"; //Call the method with an argument
     
     // Employee employee1;  employee1 is an object of the class data type Employee
     // employee1.Name = "Syrus";
